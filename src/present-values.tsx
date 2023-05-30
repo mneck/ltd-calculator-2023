620c3af1f8b54480453a853ebd6aae7911982819
Matt N <mneckelmann@gmail.com>
2023-05-30 15:57:59 +0200

    Initialize setup

---
+++ src/present-values.tsx
@@ -0,0 +1,207 @@
+// https://www.canlii.org/en/bc/laws/regu/bc-reg-352-81/latest/bc-reg-352-81.html?autocompleteStr=352%2F81&autocompletePos=1
+
+const discountValues = [
+  {
+    year: 1,
+    discountValue: 0.9852,
+  },
+  {
+    year: 2,
+    discountValue: 1.9559,
+  },
+  {
+    year: 3,
+    discountValue: 2.9122,
+  },
+  {
+    year: 4,
+    discountValue: 3.8544,
+  },
+  {
+    year: 5,
+    discountValue: 4.7826,
+  },
+  {
+    year: 6,
+    discountValue: 5.6972,
+  },
+  {
+    year: 7,
+    discountValue: 6.5982,
+  },
+  {
+    year: 8,
+    discountValue: 7.4859,
+  },
+  {
+    year: 9,
+    discountValue: 8.3605,
+  },
+  {
+    year: 10,
+    discountValue: 9.2222,
+  },
+  {
+    year: 11,
+    discountValue: 10.0711,
+  },
+  {
+    year: 12,
+    discountValue: 10.9075,
+  },
+  {
+    year: 13,
+    discountValue: 11.7315,
+  },
+  {
+    year: 14,
+    discountValue: 12.5434,
+  },
+  {
+    year: 15,
+    discountValue: 13.3432,
+  },
+
+  {
+    year: 16,
+    discountValue: 14.1313,
+  },
+  {
+    year: 17,
+    discountValue: 14.9076,
+  },
+  {
+    year: 18,
+    discountValue: 15.6726,
+  },
+  {
+    year: 19,
+    discountValue: 16.4262,
+  },
+  {
+    year: 20,
+    discountValue: 17.1686,
+  },
+  {
+    year: 21,
+    discountValue: 17.9001,
+  },
+  {
+    year: 22,
+    discountValue: 18.6208,
+  },
+  {
+    year: 23,
+    discountValue: 19.3309,
+  },
+  {
+    year: 24,
+    discountValue: 20.0304,
+  },
+  {
+    year: 25,
+    discountValue: 20.7196,
+  },
+  {
+    year: 26,
+    discountValue: 21.3986,
+  },
+  {
+    year: 27,
+    discountValue: 22.0676,
+  },
+  {
+    year: 28,
+    discountValue: 22.7267,
+  },
+  {
+    year: 29,
+    discountValue: 23.3761,
+  },
+  {
+    year: 30,
+    discountValue: 24.0158,
+  },
+  {
+    year: 31,
+    discountValue: 24.6461,
+  },
+  {
+    year: 32,
+    discountValue: 25.2671,
+  },
+  {
+    year: 33,
+    discountValue: 25.879,
+  },
+  {
+    year: 34,
+    discountValue: 26.4817,
+  },
+  {
+    year: 35,
+    discountValue: 27.0756,
+  },
+  {
+    year: 36,
+    discountValue: 27.6607,
+  },
+  {
+    year: 37,
+    discountValue: 26.2371,
+  },
+  {
+    year: 38,
+    discountValue: 28.8051,
+  },
+  {
+    year: 39,
+    discountValue: 29.3646,
+  },
+  {
+    year: 40,
+    discountValue: 29.9158,
+  },
+  {
+    year: 41,
+    discountValue: 30.459,
+  },
+  {
+    year: 42,
+    discountValue: 30.9941,
+  },
+  {
+    year: 43,
+    discountValue: 31.5212,
+  },
+  {
+    year: 44,
+    discountValue: 32.0406,
+  },
+  {
+    year: 45,
+    discountValue: 32.5523,
+  },
+  {
+    year: 46,
+    discountValue: 33.0565,
+  },
+  {
+    year: 47,
+    discountValue: 33.5532,
+  },
+  {
+    year: 48,
+    discountValue: 34.0426,
+  },
+  {
+    year: 49,
+    discountValue: 34.5247,
+  },
+  {
+    year: 50,
+    discountValue: 34.9997,
+  },
+];
+
+// export default discountValues;
