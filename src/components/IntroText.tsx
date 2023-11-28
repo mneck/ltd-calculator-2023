75790c29dad20d47c1e6dc39a7109d249f50d793
Matt N <mneckelmann@gmail.com>
2023-11-28 08:46:40 +0100

    Set up project

--- src/components/IntroText.tsx
+++ src/components/IntroText.tsx
@@ -1,7 +1,7 @@
 function IntroText() {
   return (
     <p>
-      Let's say you're disable from working and you receive a monthly amount
+      Let's say you're disabled from working and you receive a monthly amount
       from an insurance company under a private long-term disability insurance
       policy. Now let's say that insurance company (the "insurer") has decided
       to terminate your benefits, but your doctors say you are still disabled
